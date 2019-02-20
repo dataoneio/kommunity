221bdc4555f9eae25efe853b90b91dbfdaec3b80
kunalshah4345 <kunal9shah@yahoo.com>
2019-02-20 19:10:50 +0530

    After implementing accept request functionality in UserNotification.js

---
+++ files/JoiningRequests.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  //Button,
+  Dimensions
+} from "react-native";
+import { Icon } from "react-native-elements";
+import {
+  widthPercentageToDP,
+  heightPercentageToDP
+} from "react-native-responsive-screen";
+const win = Dimensions.get("window");
+export default class JoiningRequests extends Component {
+  handlepress() {
+    this.props.acceptingfunction();
+  }
+  handlepress1() {
+    alert("lmao");
+  }
+  render() {
+    return (
+      <View style={{ backgroundColor: "white" }}>
+        <View
+          style={{
+            flexDirection: "row"
+            // justifyContent: "space-evenly"
+          }}
+        >
+          <View style={{paddingRight:0, width:"75%"}}>
+            <Text
+              style={{
+
+                fontFamily: "lucida grande",
+                paddingLeft: 5,
+               // paddingRight:10,
+                fontSize: 16,
+                color: "black",
+                fontWeight: "bold",
+                borderColor: "white"
+              }}
+            >
+              {this.props.val.Name}({this.props.val.Number}) has requested to
+              join your app
+            </Text>
+          </View>
+          <View style={{flexDirection:"row", paddingLeft:40}}>
+            <View>
+              <TouchableOpacity
+                title="accept"
+                onPress={this.handlepress.bind(this)}
+              >
+                <Icon name="done" color="green" size={35} />
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              title="reject"
+              onPress={this.handlepress1.bind(this)}
+            >
+              <Icon
+                name="close"
+                type="material-community"
+                size={35}
+                color="red"
+                style={{ paddingTop: 7 }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  note: {
+    position: "relative",
+    padding: 0,
+    paddingBottom: 10,
+    backgroundColor: "#1B2936"
+    //borderBottomWidth: 2,
+    //borderBottomColor: "#ededed"
+  },
+  ImageContainer1: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 50
+  },
+
+  noteText1: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    //fontWeight: "bold",
+    fontSize: 14,
+    color: "white"
+
+    //borderLeftWidth: 5,
+    //borderLeftColor: "black"
+  },
+  noteText2: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    //fontWeight:"bold",
+    fontSize: 14,
+    color: "#8b8b8b"
+
+    //borderLeftWidth: 5,
+    //borderLeftColor: "black"
+  },
+  noteText: {
+    paddingLeft: 170,
+    paddingRight: 30,
+    color: "#8b8b8b"
+    //borderLeftWidth: 5,
+    //borderLeftColor: "black"
+  }
+});
--- files/UserNotification.js
+++ files/UserNotification.js
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
+
+import Image from "react-native-image-progress";
+import * as Progress from "react-native-progress";
+
+import { Icon } from "react-native-elements";
+import { Dropdown } from "react-native-material-dropdown";
+import {
+  CountryDropdown,
+  RegionDropdown,
+  CountryRegionData
+} from "react-country-region-selector";
 import {
   Platform,
   StyleSheet,
   Text,
   View,
-  Button,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView,
+  Button
 } from "react-native";
+import { TextInput } from "react-native-paper";
+import Joining_Requests from "./JoiningRequests";
+import firebase from "../Firebase";
+
 import SendSMS from "react-native-sms";
-import { Icon } from "react-native-elements";
 
 export default class UserNotification extends React.Component {
-  onPress() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      requests: []
+    };
+  }
+  componentDidMount() {
+    console.log("will mount");
+    this.getDataFromFirebase();
+  }
+  onPress1(Number) {
     SendSMS.send(
       {
         body: "testing for community app",
-        recipients: ["9408880345", "8511291978"],
+        recipients: [Number],
         successTypes: ["sent", "queued"],
         allowAndroidSendWithoutReadPermission: true
       },
@@ -30,6 +56,7 @@ export default class UserNotification extends React.Component {
         );
       }
     );
+    
   }
   handlenavigation() {
     alert("navigated");
@@ -37,7 +64,60 @@ export default class UserNotification extends React.Component {
   goback() {
     alert("go back");
   }
+  getDataFromFirebase() {
+    let arr1 = [];
+    var d = new Date();
+    console.log("date===" + d);
+    firebase
+      .database()
+      .ref("app/Joining_Requests")
+      .on("child_added", data => {
+        var result = [];
+        var key1 = [];
+        key1.push(data.key);
+        let arr = data.toJSON();
+        console.log("JOINING REQUESTS" + JSON.stringify(arr));
+        for (var i in arr) {
+          result.push(arr[i]);
+        }
+        console.log("key--" + key1);
+        console.log("---" + result.length);
+
+        arr1.push({
+          Number: result[0].toString(),
+          Name: result[1].toString()
+        });
+        this.setState({ requests: arr1 });
+        console.log("Number" + result[0].toString());
+        console.log("Name" + result[1].toString());
+        console.log("aaawwww" + JSON.stringify(arr1));
+      });
+  }
   render() {
+    let requestval = this.state.requests.map((val, key) => {
+      return (
+        <View key={key} style={{ padding: 5 }}>
+          <View
+            style={{
+              padding: 0,
+            
+              // borderRadius: 5,
+              // borderBottomWidth: 0.5,
+              // borderBottomColor: "white",
+              // backgroundColor: "#1B2936"
+            }}
+          >
+            <Joining_Requests
+              key={key}
+              keyval={key}
+              val={val}
+              acceptingfunction={() => this.onPress1(val.Number)}
+            />
+          </View>
+        </View>
+      );
+    });
+
     return (
       <View>
         <View style={styles.header}>
@@ -60,10 +140,20 @@ export default class UserNotification extends React.Component {
               <Icon name="edit" color="white" size={25} />
             </TouchableOpacity>
           </View>
-       
-          <Button title="presme" onPress={this.onPress.bind(this)}>
+        </View>
+        {/* <View>
+          <Button title="pressss me" onPress={this.onPress1.bind(this)}>
             press me
           </Button>
+        </View> */}
+        <View
+          style={{
+            flexWrap: "wrap-reverse",
+            flexDirection: "column-reverse",
+            backgroundColor: "transparent"
+          }}
+        >
+          {requestval}
         </View>
       </View>
     );
@@ -88,6 +178,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 18,
     color: "white"
+  },
+  header: {
+    backgroundColor: "#243545",
+    //alignItems: "center",
+    //justifyContent: "center",
+    borderBottomWidth: 2,
+    borderBottomColor: "white",
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   instructions: {
     textAlign: "center",
